8c50113ce0dae43a1204ef4aae6736eae0dcca12
Dan Kozitza <dankoz@gmx.us>
2015-03-08 18:55:19 -0500

    working on DeepTypeCheck

--- dkutils.go
+++ dkutils.go
@@ -98,3 +98,182 @@ func ForceType(v interface{}, d interface{}) error {
 
 	return nil
 }
+
+// Checker
+//
+// A checker is used by DeepTypeCheck when a pair of non-iterable variables are
+// found.
+//
+type Checker interface {
+
+	// Check
+	//
+	// the Check function will be called on any non-iterable expected, variable
+	// pair found.
+	//
+	Check(expected interface{}, variable interface{}) error
+}
+
+// DeepTypeCheck
+//
+//	This function is used to crawl through an expected or 'template' data
+// structure and compare it to an unknown data structure to check their contents
+// against one another. When non-iterable data types are found they are passed
+// to a Checker. If a Checker returns an error DeepTypeCheck stops crawling
+// and returns the error to the caller.
+//
+// TODO: A checker can also return a new variable which will replace
+//       whatever 'variable' parameter was passed in.
+//
+func DeepTypeCheck(expected interface{}, variable interface{}, c Checker) error {
+
+	if expected == nil && variable == nil {
+		fmt.Println("expected and variable are nil")
+		return nil
+	}
+	if expected == nil && variable != nil {
+		fmt.Println("only expected is nil")
+		return nil
+	}
+	if expected != nil && variable == nil {
+		fmt.Println("only variable is nil")
+		return nil
+	}
+
+	var msg string
+	etype := reflect.TypeOf(expected)
+	vtype := reflect.TypeOf(variable)
+
+	//	if conf["dkutils_DeepTypeCheck_ignore_pointers"].(int) == 0 {
+	//		if etype.String() != vtype.String() {
+	//			msg = "DeepTypeCheck: expected type " + etype.String() +
+	//				" but found type " + vtype.String() + "."
+	//
+	//			return ErrDkutilsGeneric(msg)
+	//
+	//		}
+	//	}
+
+	fmt.Println("Before pointer removal: etype: " + etype.String() + ", vtype: " + vtype.String())
+
+	// check if etype is a pointer and set evalue
+	var evalue reflect.Value
+	if etype.Kind() == reflect.Ptr {
+		fmt.Println("removed pointer to expected value")
+		evalue = reflect.Indirect(reflect.ValueOf(expected))
+		etype = reflect.TypeOf(evalue.Interface())
+
+	} else {
+		evalue = reflect.ValueOf(expected)
+	}
+
+	// same for vtype
+	var vvalue reflect.Value
+	if vtype.Kind() == reflect.Ptr {
+		fmt.Println("removed pointer to variable value")
+		vvalue = reflect.Indirect(reflect.ValueOf(variable))
+		vtype = reflect.TypeOf(vvalue.Interface())
+
+	} else {
+		vvalue = reflect.ValueOf(variable)
+	}
+
+	fmt.Println("After pointer removal: etype: ", etype, ", vtype: ", vtype)
+
+	// maybe only check kind here? check type in the default case of the kind
+	// switch.
+	//if etype != vtype {
+	//	msg = "DeepTypeCheck: expected type " + etype.String() +
+	//		" but found type " + vtype.String() + "."
+
+	//	return ErrDkutilsGeneric(msg)
+
+	//}
+
+	if etype.Kind() != vtype.Kind() {
+
+		// if the expected is a map or slice then return an error, these cannot
+		// be converted
+		switch etype.Kind() {
+		case reflect.Map, reflect.Slice:
+			msg = "DeepTypeCheck: expected reflect.Kind " +
+				fmt.Sprint(etype.Kind()) + " but found " +
+				fmt.Sprint(vtype.Kind())
+			return ErrDkutilsGeneric(msg)
+
+		default:
+			return c.Check(evalue.Interface(), vvalue.Interface())
+		}
+	}
+
+	switch etype.Kind() {
+	case reflect.Map:
+
+		newexp, ok := evalue.Interface().(map[string]interface{})
+		if !ok {
+			msg = "DeepTypeCheck: maps are expected to be of type " +
+				"map[string]interface{} not type " + etype.String()
+			return ErrDkutilsGeneric(msg)
+		}
+		newvar, ok := vvalue.Interface().(map[string]interface{})
+		if !ok {
+			msg = "DeepTypeCheck: maps are expected to be of type " +
+				"map[string]interface{} not type " + vtype.String()
+			return ErrDkutilsGeneric(msg)
+		}
+
+		fmt.Println("etype.Kind():", etype.Kind(), "vtype.Kind():", vtype.Kind())
+
+		for k, _ := range newexp {
+			fmt.Println("-\ngoing deeper into expected key:", k, ", val:", newexp[k])
+			err := DeepTypeCheck(newexp[k], newvar[k], c)
+			if err != nil {
+				return err
+			}
+		}
+
+	case reflect.Slice:
+
+		newexp, ok := evalue.Interface().([]interface{})
+		if !ok {
+			msg = "DeepTypeCheck: slices are expected to be of type " +
+				"[]interface{} not type " + etype.String()
+			return ErrDkutilsGeneric(msg)
+		}
+		newvar, ok := vvalue.Interface().([]interface{})
+		if !ok {
+			msg = "DeepTypeCheck: slices are expected to be of type " +
+				"[]interface{} not type " + vtype.String()
+			return ErrDkutilsGeneric(msg)
+		}
+
+		// check that lengths are the same
+		elen := len(newexp)
+		vlen := len(newvar)
+		if elen != vlen {
+			msg = fmt.Sprint("DeepTypeCheck: expected slice of length ", elen,
+				" but found slice of length ", vlen)
+			return ErrDkutilsGeneric(msg)
+		}
+
+		fmt.Println("etype.Kind():", etype.Kind(), "vtype.Kind:", vtype.Kind())
+
+		for i, _ := range newexp {
+			fmt.Println("-\ngoing deeper into expected index:", i, ", val:", newexp[i])
+			err := DeepTypeCheck(newexp[i], newvar[i], c)
+			if err != nil {
+				return err
+			}
+		}
+
+	default:
+
+		// now call c.Check()
+		fmt.Println("etype.Kind():", etype.Kind(), "vtype.Kind:", vtype.Kind())
+		fmt.Println("did the default")
+
+		return nil
+	}
+
+	return nil
+}
--- dkutils_test.go
+++ dkutils_test.go
@@ -73,3 +73,139 @@ func TestForceType(t *testing.T) {
 		t.Fail()
 	}
 }
+
+//func TestDeepTypeCheck(t *testing.T) {
+//
+//	fmt.Println("\nDeepTypeCheck: testing testvar1")
+//	var testvar1 interface{} = "str"
+//	err := DeepTypeCheck(testvar1, 3)
+//	if err != nil {
+//		fmt.Println("DeepTypeCheck returned error: " + err.Error())
+//
+//	} else {
+//		fmt.Println("DeepTypeCheck did not return an error!")
+//		t.Fail()
+//	}
+//
+//	if fmt.Sprint(testvar1) != fmt.Sprint(3) {
+//		fmt.Println("DeepTypeCheck did not set testvar1 to default")
+//		fmt.Println("testvar:", testvar1, "should be:", 3)
+//		t.Fail()
+//	}
+//
+//	fmt.Println("\nDeepTypeCheck: testing testvar2")
+//	var testvar2 interface{} = map[string]interface{}{
+//		"this": int64(12)}
+//		err = DeepTypeCheck(map[string]interface{}{"this": int(12)}, testvar2)
+//	if err != nil {
+//		fmt.Println("DeepTypeCheck returned error: " + err.Error())
+//		t.Fail()
+//	}
+//}
+
+func TestKind(t *testing.T) {
+	var tc TestChecker
+
+	fmt.Println("\nDeepTypeCheck: testing kind")
+	var expected4 interface{} = []interface{}{
+		new(string),
+		new(string)}
+	var testvar4 interface{} = "horse"
+	err := DeepTypeCheck(expected4, testvar4, tc)
+	if err != nil {
+		fmt.Println("DeepTypeCheck returned error: " + err.Error())
+	} else {
+		t.Fail()
+	}
+}
+
+func TestMapTypes(t *testing.T) {
+	var tc TestChecker
+
+	fmt.Println("\nDeepTypeCheck: testing map types")
+	// this setup shows that testvar3 contains int32 values!?!?
+	//var expected3 interface{} = map[string]interface{}{
+	//	"twelve": int32(12),
+	//	"three": int32(3)}
+	//var testvar3 interface{} = map[string]interface{}{
+	//	"twelve": int64(12),
+	//	"three": int64(3)}
+
+	var expected3 interface{} = map[string]interface{}{
+		"twelve": int32(12),
+		"three":  int32(3)}
+	var testvar3 interface{} = map[string]int32{
+		"twelve": 12,
+		"three":  3}
+
+	err := DeepTypeCheck(expected3, testvar3, tc)
+	if err != nil {
+		fmt.Println("DeepTypeCheck returned error: " + err.Error())
+	} else {
+		t.Fail()
+	}
+}
+
+func TestDeepMapTypes(t *testing.T) {
+	var tc TestChecker
+
+	fmt.Println("\nDeepTypeCheck: testing deep map types")
+
+	// this setup shows that testvar3 contains int32 values!?!?
+	var expected3 interface{} = map[string]interface{}{
+		"twelve": int32(12),
+		"three":  int32(3)}
+	var testvar3 interface{} = map[string]interface{}{
+		"twelve": int64(12),
+		"three":  int64(3)}
+
+	err := DeepTypeCheck(expected3, testvar3, tc)
+	if err != nil {
+		fmt.Println("DeepTypeCheck returned error: " + err.Error())
+		t.Fail()
+	}
+}
+
+func TestSliceType(t *testing.T) {
+	var tc TestChecker
+	fmt.Println("\nDeepTypeCheck: testing slice type")
+	var expected4 interface{} = []interface{}{
+		new(string),
+		new(string)}
+	var testvar4 interface{} = []*string{
+		new(string),
+		new(string)}
+	err := DeepTypeCheck(expected4, testvar4, tc)
+	if err != nil {
+		fmt.Println("DeepTypeCheck returned error: " + err.Error())
+	} else {
+		t.Fail()
+	}
+}
+
+func TestSliceLength(t *testing.T) {
+	var tc TestChecker
+	fmt.Println("\nDeepTypeCheck: testing slice length")
+	var expected4 interface{} = []interface{}{
+		new(string),
+		new(int),
+		new(int64)}
+	var testvar4 interface{} = []interface{}{}
+	//	"this": new(float64),
+	//	"these": []int{0, 1, 2}}
+	err := DeepTypeCheck(expected4, testvar4, tc)
+	if err != nil {
+		fmt.Println("DeepTypeCheck returned error: " + err.Error())
+	} else {
+		t.Fail()
+	}
+}
+
+type TestChecker struct{}
+
+func (tc TestChecker) Check(expected interface{}, variable interface{}) error {
+
+	fmt.Println("TestChecker: got expected: ", expected, " and variable: ", variable)
+
+	return nil
+}
